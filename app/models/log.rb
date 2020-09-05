4d98126470d07f140c4bc3735f0bf8fc50c2b827
sf2020-LATTE <second.s.fujii@gmail.com>
2020-09-05 15:28:15 +0900

    add-graph-function

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,2 +1,10 @@
 class Log < ApplicationRecord
+  def self.chart_date
+    #:ascはデータを昇順にする
+    order(effort: :asc).pluck('start_time', 'effort').to_h
+  end
+
+  before_save do
+    self.effort = time * tiredness
+  end
 end
